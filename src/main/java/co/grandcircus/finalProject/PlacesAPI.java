8b7492b4d0606bb9e4f957d59444133a681895a5
James McDowell <jamesmcdowell1897@gmail.com>
2020-03-13 11:12:50 -0400

    hope this works

---
+++ src/main/java/co/grandcircus/finalProject/PlacesAPI.java
@@ -0,0 +1,31 @@
+package co.grandcircus.finalProject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+public class PlacesAPI {
+
+	@Component
+	public class ApiService {
+
+		@Value("${TM-api}")
+		private String APIKey;
+
+		private RestTemplate rt;
+
+		{
+			ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+				request.getHeaders().add(HttpHeaders.USER_AGENT, "ooeeee!");
+				return execution.execute(request, body);
+			};
+			rt = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
+		}
+
+	}
+}
+
+
